2f3b4b0145336364a33bb355087188e0947f2ac5
Federico Peralta <peralta.federico.manuel@gmail.com>
2024-01-17 10:12:03 -0300

    .

--- api/src/functions/webhook.ts
+++ api/src/functions/webhook.ts
@@ -23,88 +23,97 @@ import { db } from '../lib/db'
  */
 export const handler = async (event: APIGatewayEvent, _context: Context) => {
   console.log('event.path', event.httpMethod, event.path)
-  if (event.httpMethod !== 'POST' || event.path !== '/webhook/auth0/users') {
-    return {
-      statusCode: 404,
-    }
-  }
-  const body = JSON.parse(event.body || '{}') as {
-    token: string
-    auth0Id: string
-  }
-
-  if (body.token !== 'token') {
-    return {
-      statusCode: 401,
-    }
-  }
-
-  const management = new ManagementClient({
-    domain: process.env.AUTH0_DOMAIN || '',
-    clientId: process.env.AUTH0_MANAGEMENT_CLIENT_ID || '',
-    clientSecret: process.env.AUTH0_MANAGEMENT_CLIENT_SECRET || '',
-    audience: process.env.AUTH0_MANAGEMENT_AUDIENCE || '',
-  })
-
-  let auth0User: GetUsers200ResponseOneOfInner
-
-  let newUser = await db.user.findFirst({
-    where: {
-      auth0Id: body?.auth0Id || '',
-    },
-  })
-
-  if (newUser) {
-    logger.info('Found user in db', { user: newUser })
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        data: newUser,
-      }),
-    }
-  }
-
-  try {
-    auth0User = (await management.users.get({ id: body?.auth0Id || '' })).data
-    logger.info('Found user in auth0', { user: auth0User })
-  } catch (error) {
-    logger.error('Error fetching user from auth0', { error })
-    return {
-      statusCode: 404,
-      body: JSON.stringify({
-        error: error.message,
-      }),
-    }
-  }
-
-  try {
-    newUser = await db.user.create({
-      data: {
-        email: auth0User.email,
-        name: auth0User.name,
-        photoUrl: auth0User.picture,
-        username: auth0User.nickname,
-        auth0Id: auth0User.user_id,
-        emailVerified: true,
-      },
-    })
-  } catch (error) {
-    logger.error('Error creating new user', { error })
-    throw error
-  }
-
-  logger.info('Created new user', { user: newUser })
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      ...newUser,
-    }),
+    body: JSON.stringify({ message: 'ok' }),
   }
+
+  // if (event.httpMethod !== 'POST' || event.path !== '/webhook/auth0/users') {
+  //   return {
+  //     statusCode: 404,
+  //   }
+  // }
+  // const body = JSON.parse(event.body || '{}') as {
+  //   token: string
+  //   auth0Id: string
+  // }
+
+  // if (body.token !== 'token') {
+  //   return {
+  //     statusCode: 401,
+  //   }
+  // }
+
+  // const management = new ManagementClient({
+  //   domain: process.env.AUTH0_DOMAIN || '',
+  //   clientId: process.env.AUTH0_MANAGEMENT_CLIENT_ID || '',
+  //   clientSecret: process.env.AUTH0_MANAGEMENT_CLIENT_SECRET || '',
+  //   audience: process.env.AUTH0_MANAGEMENT_AUDIENCE || '',
+  // })
+
+  // let auth0User: GetUsers200ResponseOneOfInner
+
+  // let newUser = await db.user.findFirst({
+  //   where: {
+  //     auth0Id: body?.auth0Id || '',
+  //   },
+  // })
+
+  // if (newUser) {
+  //   logger.info('Found user in db', { user: newUser })
+  //   return {
+  //     statusCode: 200,
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       data: newUser,
+  //     }),
+  //   }
+  // }
+
+  // try {
+  //   auth0User = (await management.users.get({ id: body?.auth0Id || '' })).data
+  //   logger.info('Found user in auth0', { user: auth0User })
+  // } catch (error) {
+  //   logger.error('Error fetching user from auth0', { error })
+  //   return {
+  //     statusCode: 404,
+  //     body: JSON.stringify({
+  //       error: error.message,
+  //     }),
+  //   }
+  // }
+
+  // try {
+  //   newUser = await db.user.create({
+  //     data: {
+  //       email: auth0User.email,
+  //       name: auth0User.name,
+  //       photoUrl: auth0User.picture,
+  //       username: auth0User.nickname,
+  //       auth0Id: auth0User.user_id,
+  //       emailVerified: true,
+  //     },
+  //   })
+  // } catch (error) {
+  //   logger.error('Error creating new user', { error })
+  //   throw error
+  // }
+
+  // logger.info('Created new user', { user: newUser })
+
+  // return {
+  //   statusCode: 200,
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     ...newUser,
+  //   }),
+  // }
 }
